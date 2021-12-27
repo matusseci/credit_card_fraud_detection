cf4a54e69b343e67e0e320612b2f751fe63cb148
matusseci <s1974755@ed.ac.uk>
2021-12-27 22:02:45 +0000

    Matus added developed models for the credit card fraud det

--- cc_fraud.py
+++ cc_fraud.py
@@ -27,9 +27,20 @@ from sklearn.preprocessing import StandardScaler, RobustScaler
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
 
 from sklearn.decomposition import PCA, TruncatedSVD
 from sklearn.manifold import TSNE
+
+# Classifier Libraries
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+import collections
 
 ### Read in the data
 
@@ -225,6 +236,110 @@ ax3.grid(True)
 ax3.legend(handles=[blue_patch, red_patch])
 
 plt.show()
+
+### Classification using *classifiers*
+
+# Undersampling before cross validating (prone to overfit)
+X = new_df.drop('Class', axis = 1)
+y = new_df['Class']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+# Turn the values into an array for feeding the classification algorithms.
+X_train = X_train.values
+X_test = X_test.values
+y_train = y_train.values
+y_test = y_test.values
+
+# Set a dictionary of classifiers to use 
+classifiers = {
+    "LogisiticRegression": LogisticRegression(),
+    "KNearest": KNeighborsClassifier(),
+    "Support Vector Classifier": SVC(),
+    "DecisionTreeClassifier": DecisionTreeClassifier()
+}
+
+# Implement the classifiers using a for loop 
+for key, classifier in classifiers.items():
+    classifier.fit(X_train, y_train)
+    training_score = cross_val_score(classifier, X_train, y_train, cv = 5)
+    print('Classifiers: ', classifier.__class__.__name__, 'has a training score of', 
+          round(training_score.mean(), 2) * 100, '% accuracy score')
+
+### Optimize the parameters using GridSearchCV
+
+## Logistic regression 
+
+# List parameters and options to try
+log_reg_params = {'penalty' : ['none', 'l2'], 'C' : [0.001, 0.01, 0.1, 1, 10, 100, 1000]}
+
+# Fit the GridSearchCV with the given parameters
+grid_log_reg = GridSearchCV(LogisticRegression(), log_reg_params)
+grid_log_reg.fit(X_train, y_train)
+
+# Get the best results
+log_reg = grid_log_reg.best_estimator_
+
+## kNN
+
+# List parameters and options to try
+knn_params = {"n_neighbors": list(range(2, 5, 1)), 'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']}
+
+# Fit the GridSearchCV with the given parameters
+grid_knn = GridSearchCV(KNeighborsClassifier(), knn_params)
+grid_knn.fit(X_train, y_train)
+
+# Get the best results
+knn = grid_knn.best_estimator_
+
+
+## Support Vector Classifier
+
+# List parameters and options to try
+svc_params = {'C': [0.5, 0.7, 0.9, 1], 'kernel': ['rbf', 'poly', 'sigmoid', 'linear']}
+
+# Fit the GridSearchCV with the given parameters
+grid_svc = GridSearchCV(SVC(), svc_params)
+grid_svc.fit(X_train, y_train)
+
+# SVC best estimator
+svc = grid_svc.best_estimator_
+
+## Decision Trees
+
+# List parameters and options to try
+tree_params = {"criterion": ["gini", "entropy"], "max_depth": list(range(2,4,1)), 
+              "min_samples_leaf": list(range(5,7,1))}
+
+# Fit the GridSearchCV with the given parameters
+grid_tree = GridSearchCV(DecisionTreeClassifier(), tree_params)
+grid_tree.fit(X_train, y_train)
+
+# Decision trees best estimator
+tree_clf = grid_tree.best_estimator_
+
+## Get train CV score for all the models (with oversampling)
+log_reg_score = cross_val_score(log_reg, X_train, y_train, cv=5)
+print('Logistic Regression Cross Validation Score: ', round(log_reg_score.mean() * 100, 2).astype(str) + '%')
+
+knears_score = cross_val_score(knn, X_train, y_train, cv=5)
+print('Knears Neighbors Cross Validation Score', round(knears_score.mean() * 100, 2).astype(str) + '%')
+
+svc_score = cross_val_score(svc, X_train, y_train, cv=5)
+print('Support Vector Classifier Cross Validation Score', round(svc_score.mean() * 100, 2).astype(str) + '%')
+
+tree_score = cross_val_score(tree_clf, X_train, y_train, cv=5)
+print('DecisionTree Classifier Cross Validation Score', round(tree_score.mean() * 100, 2).astype(str) + '%')
+
+
+
+
+
+
+
+
+
 
 
 
